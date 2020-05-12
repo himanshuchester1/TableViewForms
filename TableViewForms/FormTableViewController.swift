3a70146d42851d80d85a7c0a2a9350d1d4e14bc4
Himanshu Sharma <himanshu.3.sharma@jci.com>
2020-05-12 16:36:31 +0530

    Scrollble Form using TableView

--- TableViewForms/FormTableViewController.swift
+++ TableViewForms/FormTableViewController.swift
@@ -10,81 +10,53 @@ import UIKit
 
 class FormTableViewController: UITableViewController {
 
+    @IBOutlet weak var userNameTextfield: UITextField!
+    @IBOutlet weak var firstNameTextfield: UITextField!
+    @IBOutlet weak var lastNameTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var contactTextfield: UITextField!
+    @IBOutlet weak var addressTextfield: UITextField!
+   
+    var signInShowing = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+   
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    @IBAction func didTapSignIn(){
+        
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    
+    @IBAction func didTapSignUp(){
+        
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    @IBAction func toggleForm(){
+        signInShowing = !signInShowing
+    
+        if signInShowing{
+          print("Sign is showing")
+        }else{
+          print("Sign up is showing")
+        }
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        let signinHiddenrows = [2,3,5,6,7,8,9,10]
+        let signupHiddenrows = [8,11]
+        
+        if signInShowing{
+            if signinHiddenrows.contains(indexPath.row){
+                return 0
+            }
+        } else{
+            if signupHiddenrows.contains(indexPath.row){
+                return 0
+            }
+        }
+        return  super.tableView(tableView, heightForRowAt: indexPath)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
